1b5a8d6f13904ba8a72b7e17f5ebdc5ea00db57a
IgnatievMisha <131794041+IgnatievMisha@users.noreply.github.com>
2023-05-11 20:49:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -95,3 +95,49 @@ class Student(Person):
 student1=Student("mama", 21, 777)
 with open('info_student.txt', 'w') as file:
     file.write(str(student1))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   import random
+class Animal:
+    def __init__(self, species, name, age, health, hunger, happiness):
+        super().__init__()
+        self.apecies=species
+        self.name=name
+        self.age=age
+        self.health=health
+        self.hunger=hunger
+        self.happines=happines
+    def grow(self):
+        self.age+=1
+        self.health=random.randint(0, 10)
+        self.hunger=random.randint(0, 10)
+        self.happines=random.randint(0, 10)
+    def eat(self):
+        if self.hunger >=6:
+            self.health += random.randint(0, 5)
+            self.happines += random.randint(0, 5)
+            self.hunger -= random.randint(5, 7)
+        else:
+            print(self.name, "hungry")
+    def play(self):
+        pass #happines
+
+class Zoo:
+    def __init__(self):
+        super.__init__()
+        self.animal=[]
